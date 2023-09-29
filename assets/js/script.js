f69c36f5e8ef01e901b16e9c8176a34a468ee83c
Alex Pohlman <alexpohlman@Alexs-MacBook-Pro.local>
2023-09-29 18:39:19 -0400

    finished most of the js. It now displays all the info from the api to the browser

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,31 @@ const apiKey = "3acc16ffae9e45df92a064e41646355f";
 
 function displayForecast(forecastData) {
   // Display forecast
-  console.log("forecast", forecastData);
+  daysForecast.innerHTML = "";
+  forecastData.list.forEach((forecast) => {
+    const date = new Date(forecast.dt * 1000).toLocaleDateString();
+    const temperature = forecast.main.temp;
+    const description = forecast.weather[0].description;
+
+    // Create forecast card elements and append them to the daysForecast element
+    const forecastCard = document.createElement("div");
+    forecastCard.classList.add("forecast-card");
+
+    const dateElement = document.createElement("p");
+    dateElement.textContent = date;
+
+    const temperatureElement = document.createElement("p");
+    temperatureElement.textContent = `${temperature}° F`;
+
+    const descriptionElement = document.createElement("p");
+    descriptionElement.textContent = description;
+
+    forecastCard.appendChild(dateElement);
+    forecastCard.appendChild(temperatureElement);
+    forecastCard.appendChild(descriptionElement);
+
+    daysForecast.appendChild(forecastCard);
+  });
 }
 
 function getForecast(lat, lon) {
@@ -42,18 +66,20 @@ function getCoordinates(city) {
 }
 
 function displayCurrent(currentData) {
-  weatherDisplay.insertAdjacentHTML(
+  currentWeather.innerHTML = "";
+  currentWeather.insertAdjacentHTML(
     "beforeend",
     `
-  <div>
-    <h2>${currentData.name}</h2>
-    <p>${currentData.main.temp}&deg; F</p>
-  </div>
-`
+    <div>
+      <h2>${currentData.name}</h2>
+      <p>${currentData.main.temp}&deg; F</p>
+    </div>
+  `
   );
-
-  getForecast(currentData.coord);
+  getForecast(currentData.coord.lat, currentData.coord.lon);
 }
+
+const cityNameElement = document.createElement("h2");
 
 function getCurrentWeather() {
   const city = cityInput.value;
@@ -61,10 +87,15 @@ function getCurrentWeather() {
 
   fetch(url)
     .then((res) => res.json())
-    .then(displayCurrent);
+    .then((data) => {
+      cityNameElement.textContent = data.name;
+      currentWeather.appendChild(cityNameElement);
+      displayCurrent(data);
+    });
 }
 
 searchBtn.addEventListener("click", () => {
   console.log(cityInput.value);
   getCoordinates(cityInput.value);
+  getCurrentWeather();
 });
